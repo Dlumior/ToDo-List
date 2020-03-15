327244fdccf5ef9e159d7987bfb421659ab65e42
Luis Ramirez Osorio <dlumior@localhost.localdomain>
2020-03-15 14:05:13 -0500

    add feat of add task

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def homepage():
-    return "Hello World!"
+tasks = []
 
+@app.route("/", methods=["GET"])
+def homepage():
+    return render_template("todolist.html", tasks=tasks)
+
+@app.route("/", methods=["POST"])
+def addtask():
+    task = request.form.get("txtTask")
+    tasks.append(task)
+    return redirect(url_for("homepage"))
 
 if __name__=="__main__":
     app.run(debug=True)
